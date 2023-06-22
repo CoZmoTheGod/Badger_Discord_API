4775e3e9d59edc8078eff01cc8b17079a7179dd0
CoZmoTheGod <37917612+CoZmoTheGod@users.noreply.github.com>
2023-06-22 00:17:48 -0500

    Remove yucky advertisment

--- config.lua
+++ config.lua
@@ -5,18 +5,20 @@ Config = {
 		["name"] = "guild_id", -- Replace this with a name, like "main"
 	},
 	Bot_Token = '',
-	RoleList = {},
+	RoleList = {
+		['Member'] = 0000000000000000,
+	},
 	DebugScript = false,
 	CacheDiscordRoles = true, -- true to cache player roles, false to make a new Discord Request every time
 	CacheDiscordRolesTime = 60, -- if CacheDiscordRoles is true, how long to cache roles before clearing (in seconds)
 }
 
 Config.Splash = {
-	Header_IMG = 'https://forum.cfx.re/uploads/default/original/3X/a/6/a6ad03c9fb60fa7888424e7c9389402846107c7e.png',
+	Header_IMG = 'https://i.imgur.com/kguDPQi.jpg',
 	Enabled = true,
-	Wait = 10, -- How many seconds should splash page be shown for? (Max is 12)
-	Heading1 = "Welcome to [ServerName]",
+	Wait = 15, -- How many seconds should splash page be shown for? (Max is 12)
+	Heading1 = "Welcome to [Server Name]",
 	Heading2 = "Make sure to join our Discord and check out our website!",
-	Discord_Link = 'https://discord.gg',
-	Website_Link = 'https://badger.store',
+	Discord_Link = 'https://discord.gg/santos',
+	Website_Link = 'https://store.santosrp.xyz',
 }
